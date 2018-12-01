052cc5d05c8824c1f1f63ffa29e20f7d01323be9
twheelertech <tony@wheelertechconsulting.com>
2018-12-01 15:28:22 -0600

    Added Variants

--- fashion theme/assets/theme.js
+++ fashion theme/assets/theme.js
@@ -1,38 +1,74 @@
-$('.btn.edit-address').click(function() {
+$(".btn.edit-address").click(function() {
   $(this).hide();
-  $(this).parents('td').siblings('.address-edit').show();
-  $(this).siblings('.btn.edit-address-cancel').show();
-  $(this).siblings('.btn.edit-address-submit').show();
-  $(this).siblings('.btn.delete-address').hide();
-  $(this).parents('td').siblings('.address-record').hide()
+  $(this)
+    .parents("td")
+    .siblings(".address-edit")
+    .show();
+  $(this)
+    .siblings(".btn.edit-address-cancel")
+    .show();
+  $(this)
+    .siblings(".btn.edit-address-submit")
+    .show();
+  $(this)
+    .siblings(".btn.delete-address")
+    .hide();
+  $(this)
+    .parents("td")
+    .siblings(".address-record")
+    .hide();
 });
 
-$('.btn.edit-address-submit').click(function() {
+$(".btn.edit-address-submit").click(function() {
   $(this).hide();
-  $(this).parents('td').siblings('.address-edit').hide();
-  $(this).siblings('.btn.edit-address-cancel').hide();
-  $(this).siblings('.btn.edit-address').show();
-  $(this).siblings('.btn.delete-address').show();
-  $(this).parents('td').siblings('.address-record').show()
+  $(this)
+    .parents("td")
+    .siblings(".address-edit")
+    .hide();
+  $(this)
+    .siblings(".btn.edit-address-cancel")
+    .hide();
+  $(this)
+    .siblings(".btn.edit-address")
+    .show();
+  $(this)
+    .siblings(".btn.delete-address")
+    .show();
+  $(this)
+    .parents("td")
+    .siblings(".address-record")
+    .show();
 });
 
-$('.btn.edit-address-cancel').click(function() {
+$(".btn.edit-address-cancel").click(function() {
   $(this).hide();
-  $(this).parents('td').siblings('.address-edit').hide();
-  $(this).siblings('.btn.edit-address-submit').hide();
-  $(this).siblings('.btn.edit-address').show();
-  $(this).siblings('.btn.delete-address').show();
-  $(this).parents('td').siblings('.address-record').show()
+  $(this)
+    .parents("td")
+    .siblings(".address-edit")
+    .hide();
+  $(this)
+    .siblings(".btn.edit-address-submit")
+    .hide();
+  $(this)
+    .siblings(".btn.edit-address")
+    .show();
+  $(this)
+    .siblings(".btn.delete-address")
+    .show();
+  $(this)
+    .parents("td")
+    .siblings(".address-record")
+    .show();
 });
 
-$('.carousel').carousel()
+$(".carousel").carousel();
 
-$('.single-product-images').slick({
+$(".single-product-images").slick({
   slidesToShow: 1,
   slidesToScroll: 1,
   dots: true,
-  customPaging : function(slider, i) {
-    var thumb = $(slider.$slides[i]).data('thumb');
+  customPaging: function(slider, i) {
+    var thumb = $(slider.$slides[i]).data("thumb");
     return `<a><img class="img-fluid" src=${thumb} /></a>`;
   },
   responsive: [
@@ -63,10 +99,10 @@ $('.single-product-images').slick({
 });
 
 function getVariantFromOptions() {
-  let variantArr = []
+  let variantArr = [];
   $(".product-category select").map(function(i, el) {
-    variant = {value: $(el).val(), index: $(el).data('index')};
-    variantArr.push(variant)
+    variant = { value: $(el).val(), index: $(el).data("index") };
+    variantArr.push(variant);
   });
   return variantArr;
 }
@@ -78,26 +114,25 @@ function updateHistoryState(variant) {
 
   var newurl =
     window.location.protocol +
-    '//' +
+    "//" +
     window.location.host +
     window.location.pathname +
-    '?variant=' +
+    "?variant=" +
     variant.id;
-  
-  window.history.replaceState({ path: newurl }, '', newurl);
+
+  window.history.replaceState({ path: newurl }, "", newurl);
 }
 
-$('.product-category select').on('change', function() {
+$(".product-category select").on("change", function() {
   var selectedValues = getVariantFromOptions();
   var variants = window.product.variants;
-  
+
   // Search for product variants based on what was selected in the dropdowns
   var found = _.find(variants, function(variant) {
     return selectedValues.every(function(values) {
       return _.isEqual(variant[values.index], values.value);
     });
   });
-  updateHistoryState(found)
-  $('#variant-id').val(found.id)
+  updateHistoryState(found);
+  $("#variant-id").val(found.id);
 });
-
